086969e45bade2cfcc66ff1fe42d59a4b6dd757c
bnisevic <bojan.programer@gmail.com>
2018-02-13 15:52:50 +0100

    Added missing migration file.

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -14,16 +14,10 @@ class UserAdmin(DjangoUserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
-    )
+    add_fieldsets = ((None, {'classes': ('wide', ), 'fields': ('email', 'password1', 'password2'), }), )
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email',)
+    ordering = ('email', )
