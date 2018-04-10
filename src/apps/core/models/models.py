9ecc39f1f6243dfdd634fe8bb2ba56f94bf0ca54
vojin-nisevic <nisevic.vojin@gmail.com>
2018-04-10 13:21:11 +0200

    User profile ready for test

--- src/apps/core/models/models.py
+++
@@ -1,7 +0,0 @@
-# from django.contrib.auth.models import User
-from django.db import models
-from core.models.user import User
-
-
-class MyProfile(models.Model):
-    user = User.username
--- src/apps/core/views/views.py
+++ src/apps/core/views/views.py
@@ -1,19 +1,32 @@
 from django.views.generic import UpdateView
-from core.models.models import MyProfile
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
 from core.models.user import User
 
 
+@method_decorator(login_required, name='dispatch')
 class Profile(UpdateView):
     model = User
+    template_name = 'profile.html'
+    fields = ['first_name', 'last_name', 'birth_date', 'gender', 'profile_image', 'address', 'zip', 'city', 'state_province',
+              'country', 'home_phone', 'desk_phone', 'cell_phone']
 
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get the context
+    def get_success_url(self):
+        return reverse_lazy('profile', args=(self.pk_url_kwarg,))
 
-        context = super(Profile, self).get_context_data(**kwargs)
+    def get_object(self, queryset=None):
+        """
 
-        # Create any data and add it to the context
+        With returning current user object we prevent users from updating other user profiles.
 
-        context['email'] = User.email
-        context['name'] = User.first_name
 
-        return context
+
+        :param queryset:
+
+        :return: object User from request
+
+        """
+
+        return self.request.user
+
