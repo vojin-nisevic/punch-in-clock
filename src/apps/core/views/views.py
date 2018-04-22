59d406e055f74ef87b999cd502dd3b041c746eb4
vojin-nisevic <nisevic.vojin@gmail.com>
2018-04-22 21:55:22 +0200

    Invite employee finished. Waiting for registration

--- src/apps/core/views/views.py
+++ src/apps/core/views/views.py
@@ -2,7 +2,6 @@ from django.views.generic import UpdateView, FormView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
-from django.core.mail import send_mail, EmailMessage
 from django.conf import settings
 from django.shortcuts import redirect
 from django.core import mail
@@ -15,9 +14,6 @@ class Profile(UpdateView):
     model = User
     form_class = UserForm
     template_name = 'profile.html'
-    # fields = ['first_name', 'last_name', 'birth_date', 'gender', 'profile_image', 'address',
-    # 'zip', 'city', 'state_province',
-    # 'country', 'home_phone', 'desk_phone', 'cell_phone']
 
     def get_success_url(self):
         return reverse_lazy('profile', args=(self.pk_url_kwarg,))
@@ -47,20 +43,8 @@ class Invite(FormView):
         message = form.cleaned_data['message']
         from_email = settings.DEFAULT_FROM_EMAIL
         to_email = form.cleaned_data['recipient']
-        # emails = []
-        # if ',' in to_email:
-        #     for email in to_email.split(','):
-        #         emails.append(email)
-        #     else:
-        #         emails.append(to_email)
-        # mail = EmailMessage(subject, message, from_email, emails)
-        # if 'send_email' in self.request.POST:
-        #     send_mail(subject, message, from_email, emails, fail_silently=False)
-        #  mail.send()
         with mail.get_connection() as connection:
             mail.EmailMessage(subject, message, from_email, [to_email], connection=connection).send(fail_silently=False)
         return redirect('home')
 
-    # def get_success_url(self):
-    #     return reverse_lazy('home')
 
