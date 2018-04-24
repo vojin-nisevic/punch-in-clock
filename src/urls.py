88639facf8f0448f66423a8c0898bdc94d4bb473
vojin-nisevic <nisevic.vojin@gmail.com>
2018-04-24 20:56:02 +0200

    Logout finished: documentation example

--- src/urls.py
+++ src/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from django.conf import settings
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
-from core.views.views import Profile, logout
+from core.views.views import Profile
 
 
 urlpatterns = [
