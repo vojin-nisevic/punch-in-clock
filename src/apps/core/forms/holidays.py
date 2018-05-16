f99b17966476caaf1ec01cff59ead947069b9cdf
vojin-nisevic <nisevic.vojin@gmail.com>
2018-05-16 22:19:31 +0200

    Added message after deleting holiday

--- src/apps/core/forms/holidays.py
+++ src/apps/core/forms/holidays.py
@@ -1,10 +1,8 @@
-from django.forms import ModelForm, DateField
-from django.conf import settings
+from django.forms import ModelForm
 from core.models.holidays import Holidays
 
 
 class HolidayForm(ModelForm):
-    # date = DateField(input_formats=settings.DATE_INPUT_FORMATS)
 
     class Meta:
         model = Holidays
